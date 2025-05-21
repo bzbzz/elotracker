4719e7543482e7c12ac9000b35d0133f07c43c76
Nathan <bzbzz@2a01cb06904dace0c6bf7427314603cd.ipv6.abo.wanadoo.fr>
2025-05-21 14:21:58 +0200

    fix: following the doc to set up prisma vercel supa

---
+++ prisma/migrations/0_init_supabase/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE "Player" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "startingElo" INTEGER NOT NULL,
+    "goalElo" INTEGER NOT NULL,
+    "mode" TEXT NOT NULL,
+
+    CONSTRAINT "Player_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "EloSnapshot" (
+    "id" SERIAL NOT NULL,
+    "playerId" INTEGER NOT NULL,
+    "elo" INTEGER NOT NULL,
+    "recorded" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "progress" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "EloSnapshot_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Player_username_key" ON "Player"("username");
+
+-- AddForeignKey
+ALTER TABLE "EloSnapshot" ADD CONSTRAINT "EloSnapshot_playerId_fkey" FOREIGN KEY ("playerId") REFERENCES "Player"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
